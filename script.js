24bb31562e68920ea6b03380ee527e0f1fa83d1c
Aliceman93 <153220998+Aliceman93@users.noreply.github.com>
2024-03-14 18:37:11 +0300

    Домашнее задание 10

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
 const form = document.querySelector(".form");
 form.addEventListener("submit", (event) => {
-  // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
-  // https://learn.javascript.ru/default-browser-action
-  event.preventDefault();
+    // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
+    // https://learn.javascript.ru/default-browser-action
+    event.preventDefault();
 
-  // Здесь твой код
-});
+    // Здесь твой код
+    const sendButton = document.querySelector("#button");
+
+
+    sendButton.addEventListener('click', () => {
+
+        fetch(`https://polinashneider.space/user`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer: Aliceman93'
+            },
+            body: JSON.stringify({
+                "name": document.querySelector("#name").value,
+                "secondName": document.querySelector("#secondName").value,
+                "phone": document.querySelector("#phone").value,
+                "email": document.querySelector("#email").value,
+                "agree": document.querySelector("#agree").value
+            }),
+        }).then((result) => {
+            return result.json()
+        }).then((data) => {
+            document.getElementById("clear").reset();
+            console.log(data);
+            alert('Ваш ответ принят')
+        }).catch((error) => {
+            console.log(error)
+            alert('Попробуйте еще раз')
+        });
+
+    })
+})
