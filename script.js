89a4736c980d5221ba8aac332639eea61ca670a5
Alexandrovna7 <dina25alexandrovna05@gmail.com>
2024-07-28 14:43:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,35 @@ form.addEventListener("submit", (event) => {
   // https://learn.javascript.ru/default-browser-action
   event.preventDefault();
 
-  // Здесь твой код
+const sendButton = document.querySelector('#button');
+const clearButton = document.querySelector('#clear');
+
+sendButton.addEventListener('click', () => {
+  fetch(`https://polinashneider.space/user`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer: PolinaShneider'
+    },
+    body: JSON.stringify({
+      "name": "Полина",
+      "secondName": "Shneider",
+      "phone": 89990000000,
+      "email": "polina@gmail.com",
+      "agree": true
+    }),
+})
+  .then((result) => {
+    console.log(result);
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+})
+  
 });
+
