36a7be2ce02307ad9022fe87ca5342746033616d
Ansirotko <ansirphoto@gmail.com>
2024-11-24 23:29:51 +0300

    update hometask-10

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 const form = document.querySelector(".form");
+const nameUser = document.getElementById('name');
+const secondName = document.getElementById('secondName');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const agree = document.getElementById('agree');
+
+function makeNotification(message) {
+    const notification = document.createElement('div');
+    notification.textContent = message;
+    notification.style.position = 'fixed';
+    notification.style.bottom = '30px';
+    notification.style.right = '30px';
+    notification.style.backgroundColor = '#FD51AF';
+    notification.style.color = 'white';
+    notification.style.padding = '15px';
+    notification.style.borderRadius = '4px';
+    notification.style.zIndex = '1000';
+    document.body.appendChild(notification);
+    setTimeout(() => {
+        notification.remove();
+    }, 3000);
+}
+
 form.addEventListener("submit", (event) => {
     // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
     // https://learn.javascript.ru/default-browser-action
     event.preventDefault();
 
-    const name = document.getElementById('name').value;
-    const secondName = document.getElementById('secondName').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
-    const agree = document.getElementById('agree').checked;
-
-    function makeNotification(message) {
-        const notification = document.createElement('div');
-        notification.textContent = message;
-        notification.style.position = 'fixed';
-        notification.style.bottom = '30px';
-        notification.style.right = '30px';
-        notification.style.backgroundColor = '#FD51AF';
-        notification.style.color = 'white';
-        notification.style.padding = '15px';
-        notification.style.borderRadius = '4px';
-        notification.style.zIndex = '1000';
-        document.body.appendChild(notification);
-        setTimeout(() => {
-            notification.remove();
-        }, 3000);
-    }
     fetch(`https://polinashneider.space/user`, {
             method: 'POST',
             headers: {
@@ -34,11 +35,11 @@ form.addEventListener("submit", (event) => {
                 'Authorization': 'Bearer: Ansirotko'
             },
             body: JSON.stringify({
-                "name": name,
-                "secondName": secondName,
-                "phone": phone,
-                "email": email,
-                "agree": agree
+                "name": nameUser.value,
+                "secondName": secondName.value,
+                "phone": phone.value,
+                "email": email.value,
+                "agree": agree.checked
             }),
         })
         .then(response => {
